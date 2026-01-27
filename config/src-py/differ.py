1ee115950dfaa93a2a04e41bc3101ac42db4962c
Felipe Arnt <102182726+FelipeArnt@users.noreply.github.com>
2026-01-27 09:54:21 -0300

    Update differ.py

--- config/src-py/differ.py
+++ config/src-py/differ.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Script para comparação linha a linha de arquivos e comparação recursiva de diretórios. LABELO / VSW  
 Author: FelipeArnt
@@ -218,4 +219,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
